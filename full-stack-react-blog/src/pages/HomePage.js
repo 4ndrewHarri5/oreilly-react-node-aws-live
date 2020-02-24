8c823e2d71c69f155c6f114042b5e1f64ce5c0cf
Andrew Harris <4ndrew.harri5@gmail.com>
2020-02-24 13:08:07 +0000

    adds react router dom for routing to specific pages

---
+++ full-stack-react-blog/src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>
+            Ei dicat veniam est, prima nihil suscipit his ad, quot autem ad cum. Cum ut clita nusquam oportere, at has utroque conceptam, debitis fuisset cum ad. Et mel accusam consulatu repudiandae. Mel an quaeque signiferumque.
+        </p>
+        <p>
+            Duo putent conclusionemque ea, cum ut malorum facilis incorrupte, vel ex atqui viderer. Mea et saepe nostrud diceret. Te est duis dolores dissentias, brute delectus his ea. Ei offendit assueverit definitiones quo, ut pri tempor similique disputationi, ut nec solet adipisci.
+        </p>
+        <p>
+            Diam oblique sit an, eam falli facete lucilius et. Quo erat accusata ad, vitae convenire ea ius. Eu deleniti vituperatoribus his, quot solum est no. Vel et everti melius convenire, ei tincidunt dissentiunt has, atomorum eleifend reprehendunt ea eos. Mei ignota quidam singulis id, per probo phaedrum deserunt ne, ad erat evertitur urbanitas eam. Qui purto labore similique in, in vix audire senserit. Ridens dolorum pertinacia ex pro, ad elit laboramus eum.
+        </p>
+        <p>
+            Simul dicant officiis qui in, nam summo nihil in, noster definitiones nam ut. Sea laoreet perpetua te, at pri meliore probatus inimicus. Utamur voluptaria omittantur an eam, dolores expetenda torquatos mei id, ei cum essent quaestio. Has persius partiendo dignissim id, an aeterno vulputate vix, ei nam quot dicit. Quo ad vocent convenire deseruisse.
+        </p>
+        <p>
+            Habeo libris splendide eam te, usu et vidisse hendrerit. Similique abhorreant scribentur sed te, duo enim principes ex. Mei posse choro utinam ei, quem eius animal his an, et cum causae voluptua salutandi. Ei doctus debitis sed. Duo dolorem concludaturque an, ei ius nobis nullam delectus, id deleniti detraxit sit.
+        </p>
+    </>
+);
+
+export default HomePage;
